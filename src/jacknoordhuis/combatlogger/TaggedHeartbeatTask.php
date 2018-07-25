a220345f386163f5668625cbb275d939b98b4ca0
CupidonSauce173 <antoinelangevin999@gmail.com>
2018-07-25 14:02:32 -0400

    Update TaggedHeartbeatTask.php

--- src/jacknoordhuis/combatlogger/TaggedHeartbeatTask.php
+++ src/jacknoordhuis/combatlogger/TaggedHeartbeatTask.php
@@ -22,24 +22,25 @@ use pocketmine\scheduler\Task;
 
 class TaggedHeartbeatTask extends Task {
 
-	/**
-	 * @return CombatLogger
-	 */
-	public function getPlugin() {
-		return $this->getOwner();
+	private $plugin;
+	
+	public function __construct(CombatLogger $plugin) {
+		$this->plugin = $plugin;
+		return;
 	}
 
+	
 	public function onRun(int $currentTick) {
-		$plugin = $this->getPlugin();
-		foreach($plugin->taggedPlayers as $name => $time) {
+		
+		foreach($this->plugin->taggedPlayers as $name => $time) {
 			$time--;
 			if($time <= 0) {
-				$plugin->setTagged($name, false);
-				$player = $plugin->getServer()->getPlayerExact($name);
-				if($player instanceof Player) $player->sendMessage($plugin->getMessageManager()->getMessage("player-tagged-timeout"));
+				$this->plugin->setTagged($name, false);
+				$player = $this->plugin->getServer()->getPlayerExact($name);
+				if($player instanceof Player) $player->sendMessage($this->plugin->getMessageManager()->getMessage("player-tagged-timeout"));
 				return;
 			}
-			$plugin->taggedPlayers[$name]--;
+			$this->plugin->taggedPlayers[$name]--;
 		}
 	}
 
